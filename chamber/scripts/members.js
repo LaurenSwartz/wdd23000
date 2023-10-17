347a07c6834db543d8ee934ab42439b6fc05f181
LaurenSwartz <swa21010@byui.edu>
2023-10-17 16:15:17 +0200

    directory

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,4 +1,4 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const url = 'https://laurenswartz.github.io/wdd230/chamber/data/members.json';
 
 async function getProphetData(){
     const response = await fetch(url);
@@ -10,20 +10,23 @@ async function getProphetData(){
 getProphetData();
 
 const displayProphets = (prophets) => {
-    const cards = document.querySelector('div.cards');
+    const cards = document.querySelector('article.mani-grid');
 
     prophets.forEach((prophet) => {
         // create elements to add to the div.cards element
         let card = document.createElement('section');
+        let portrait = document.createElement('img');
         let h2 = document.createElement('h3');
         let h3 = document.createElement('h3');
         let p = document.createElement('p');
-        let portrait = document.createElement('img');
+        let url =document.createElement('url');
+        
 
         // Build the h2 content out to show the prophets full name - finish the template string
-        h2.textContent =`${prophet.name} ${prophet.lastname}`;
-        h3.textContent =`Birth: ${prophet.birthdate}`;
-        p.textContent =`Place: ${prophet.birthplace}`;
+        h2.textContent =`${prophet.name}`;
+        h3.textContent =`Address: ${prophet.address}`;
+        p.textContent =`Phone Number: ${prophet.phonenumber}`;
+        url.textContent = `${prophet.websiteurl}`;
 
         //Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src',prophet.imageurl);
@@ -33,13 +36,17 @@ const displayProphets = (prophets) => {
         portrait.setAttribute('height','440');
 
         //Append the section (card) with the created elements
+        card.appendChild(portrait);
         card.appendChild(h2);
         card.appendChild(h3);
         card.appendChild(p);
-        card.appendChild(portrait);
+        card.appendChild(url);
+        
         
         cards.appendChild(card);
 
         
     });
+
+    
 }
